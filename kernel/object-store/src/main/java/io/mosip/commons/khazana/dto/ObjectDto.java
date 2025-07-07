ecc5b6100fdb07f9c61ee58a5ca5d0ded5a44e61
TRIALBLAZERS <84778104+trialblazerseee@users.noreply.github.com>
2025-07-07 13:20:38 +0530

    MOSIP-42125 - Performance Improvement - Khazana (#113)
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
    Signed-off-by: Ashok Kumar Sharma <ashok@mosip.io>

--- kernel/object-store/src/main/java/io/mosip/commons/khazana/dto/ObjectDto.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/dto/ObjectDto.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ObjectDto {
+public class ObjectDto implements Serializable {
 
     private String source;
     private String process;
