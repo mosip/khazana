165ce1e0d4557ad9ae9bfbfc66a6441890c2c9dc
Dhanendra Sahu <60607841+dhanendra06@users.noreply.github.com>
2026-08-04 11:14:39 +0530

    github-issue#2326 (#244)
    * Added new method for copy and replace
    
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>
    
    * Updated the code as per the review comments
    
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>
    
    * handled the try catch block for moveObject method
    
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>
    
    ---------
    
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- kernel/object-store/src/main/java/io/mosip/commons/khazana/impl/S3Adapter.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/impl/S3Adapter.java
@@ -56,6 +56,7 @@ import software.amazon.awssdk.services.s3.model.S3Object;
 
 import io.mosip.commons.khazana.config.LoggerConfiguration;
 import io.mosip.commons.khazana.dto.ObjectDto;
+import io.mosip.commons.khazana.dto.ObjectStoreReference;
 import io.mosip.commons.khazana.exception.ObjectStoreAdapterException;
 import io.mosip.commons.khazana.spi.ObjectStoreAdapter;
 import io.mosip.commons.khazana.util.ObjectStoreUtil;
@@ -534,6 +535,69 @@ public class S3Adapter implements ObjectStoreAdapter, DisposableBean {
                     "Exception occured to deleteObject for : " + container,
                     ExceptionUtils.getStackTrace(e));
             throw e;
+        }
+    }
+
+    @Override
+    public boolean moveObject(ObjectStoreReference src, ObjectStoreReference dst,
+                              boolean deleteSourceAfterCopy) {
+        String srcBucketName = "";
+        String srcObjectName = "";
+        String dstBucketName = "";
+        String dstObjectName = "";
+        try {
+            if (useAccountAsBucketname) {
+                srcBucketName = normalizeBucket(src.getAccount());
+                srcObjectName = ObjectStoreUtil.getName(src.getContainer(), src.getSource(), src.getProcess(), src.getObjectName());
+            } else {
+                srcBucketName = normalizeBucket(src.getContainer());
+                srcObjectName = ObjectStoreUtil.getName(src.getSource(), src.getProcess(), src.getObjectName());
+            }
+
+            if (useAccountAsBucketname) {
+                dstBucketName = normalizeBucket(dst.getAccount());
+                dstObjectName = ObjectStoreUtil.getName(dst.getContainer(), dst.getSource(), dst.getProcess(), dst.getObjectName());
+            } else {
+                dstBucketName = normalizeBucket(dst.getContainer());
+                dstObjectName = ObjectStoreUtil.getName(dst.getSource(), dst.getProcess(), dst.getObjectName());
+            }
+
+            long startTime = System.currentTimeMillis();
+
+            getConnection(srcBucketName).copyObject(CopyObjectRequest.builder()
+                    .sourceBucket(srcBucketName)
+                    .sourceKey(srcObjectName)
+                    .destinationBucket(dstBucketName)
+                    .destinationKey(dstObjectName)
+                    .build());
+            LOGGER.debug(SESSIONID, REGISTRATIONID,
+                    "moveObject", "[S3-PERF] s3Operation: copyObject - timeTaken: "
+                            + (System.currentTimeMillis() - startTime)
+                            + " ms - srcBucket: " + srcBucketName + " - srcKey: " + srcObjectName
+                            + " - dstBucket: " + dstBucketName + " - dstKey: " + dstObjectName);
+            if (deleteSourceAfterCopy) {
+                getConnection(srcBucketName).deleteObject(
+                        DeleteObjectRequest.builder().bucket(srcBucketName).key(srcObjectName).build());
+                LOGGER.debug(SESSIONID, REGISTRATIONID,
+                        "moveObject", "[S3-PERF] s3Operation: deleteObject (source) - timeTaken: "
+                                + (System.currentTimeMillis() - startTime)
+                                + " ms - srcBucket: " + srcBucketName + " - srcKey: " + srcObjectName);
+            }
+            return true;
+        } catch (S3Exception e) {
+            LOGGER.error(SESSIONID, REGISTRATIONID,
+                    "S3 error in moveObject from: " + srcObjectName + " to: " + dstObjectName
+                            + " | status: " + e.statusCode(),
+                    ExceptionUtils.getStackTrace(e));
+            throw new ObjectStoreAdapterException(OBJECT_STORE_NOT_ACCESSIBLE.getErrorCode(),
+                    OBJECT_STORE_NOT_ACCESSIBLE.getErrorMessage(), e);
+        } catch (Exception e) {
+            shutdownConnection();
+            LOGGER.error(SESSIONID, REGISTRATIONID,
+                    "Unexpected error in moveObject from: " + srcObjectName + " to: " + dstObjectName,
+                    ExceptionUtils.getStackTrace(e));
+            throw new ObjectStoreAdapterException(OBJECT_STORE_NOT_ACCESSIBLE.getErrorCode(),
+                    OBJECT_STORE_NOT_ACCESSIBLE.getErrorMessage(), e);
         }
     }
 
--- kernel/object-store/src/main/java/io/mosip/commons/khazana/spi/ObjectStoreAdapter.java
+++ kernel/object-store/src/main/java/io/mosip/commons/khazana/spi/ObjectStoreAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import io.mosip.commons.khazana.dto.ObjectDto;
+import io.mosip.commons.khazana.dto.ObjectStoreReference;
 
 public interface ObjectStoreAdapter {
 
@@ -37,4 +38,19 @@ public interface ObjectStoreAdapter {
 	public Map<String, String> getTags(String account, String container);
 	
 	public void deleteTags(String account, String container, List<String> tags);
+
+	/**
+	 * Copies the object identified by {@code src} to the location identified by
+	 * {@code dst}. If {@code deleteSourceAfterCopy} is {@code true}, the source
+	 * object is deleted after a successful copy, effectively performing a move.
+	 *
+	 * @param src                  reference to the source object
+	 * @param dst                  reference to the destination object
+	 * @param deleteSourceAfterCopy whether to delete the source after a successful copy
+	 * @return {@code true} if the operation succeeded; {@code false} otherwise
+	 */
+	public default boolean moveObject(ObjectStoreReference src, ObjectStoreReference dst,
+			boolean deleteSourceAfterCopy) {
+		return false;
+	}
 }
